7c13c22f9a484fe52a72fc9fbc28c6ba69856522
Pepijn Schmitz <codemaster@pepsoft.org>
2025-04-06 14:05:23 +0200

    Add transient property

--- src/main/java/org/pepsoft/worldpainter/vo/EventVO.java
+++ src/main/java/org/pepsoft/worldpainter/vo/EventVO.java
@@ -13,41 +13,66 @@ import java.util.Map.Entry;
 import static java.util.stream.Collectors.toMap;
 
 /**
+ * A WorldPainter event.
  *
  * @author pepijn
  */
 public final class EventVO implements Serializable {
+    /**
+     * Create a new event with key {@code key}.
+     */
     public EventVO(String key) {
         this.key = key;
     }
-    
+
+    /**
+     * Set the {@link #ATTRIBUTE_COUNT} attribute and return the event for method chaining.
+     */
     public EventVO count(long count) {
         setAttribute(ATTRIBUTE_COUNT, count);
         return this;
     }
-    
+
+    /**
+     * Set the {@link #ATTRIBUTE_DURATION} attribute and return the event for method chaining.
+     */
     public EventVO duration(long duration) {
         setAttribute(ATTRIBUTE_DURATION, duration);
         return this;
     }
 
+    /**
+     * Set the {@link #ATTRIBUTE_TIMESTAMP} attribute and return the event for method chaining.
+     */
     public EventVO addTimestamp() {
         setAttribute(ATTRIBUTE_TIMESTAMP, new Date());
         return this;
     }
 
+    /**
+     * Get the key of the event.
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * Get a copy of the attributes currently stored on the event indexed by name.
+     */
     public Map<String, Serializable> getAttributes() {
         return (attributes != null) ? attributes.entrySet().stream().collect(toMap(entry -> entry.getKey().getKey(), Entry::getValue)) : null;
     }
 
+    /**
+     * Replace all attributes currently stored on the event with a new set of attributes indexed by name.
+     */
     public void setAttributes(Map<String, Serializable> attributes) {
         this.attributes = (attributes != null) ? attributes.entrySet().stream().collect(toMap(entry -> new AttributeKeyVO<>(entry.getKey()), Entry::getValue)) : null;
     }
 
+    /**
+     * Set an arbitrary attribute indexed by {@link AttributeKeyVO}.
+     */
     public <T extends Serializable> EventVO setAttribute(AttributeKeyVO<T> key, T value) {
         if (value != null) {
             if (attributes == null) {
@@ -62,7 +87,10 @@ public final class EventVO implements Serializable {
         }
         return this;
     }
-    
+
+    /**
+     * Get an arbitrary attribute indexed by {@link AttributeKeyVO}.
+     */
     @SuppressWarnings("unchecked")
     public <T extends Serializable> T getAttribute(AttributeKeyVO<T> key) {
         if (attributes != null) {
@@ -72,6 +100,22 @@ public final class EventVO implements Serializable {
         }
     }
 
+    /**
+     * Set the {@code transient} property to {@code true} and return the event for method chaining.
+     */
+    public EventVO setTransient() {
+        _transient = true;
+        return this;
+    }
+
+    /**
+     * Indicates whether the event is transient, meaning that it should not be stored or transmitted, only broadcast to
+     * any registered event listeners.
+     */
+    public boolean isTransient() {
+        return _transient;
+    }
+
     @Override
     public String toString() {
         return "EventVO{" + "key=" + key + ", attributes=" + attributes + '}';
@@ -79,9 +123,19 @@ public final class EventVO implements Serializable {
     
     private final String key;
     private Map<AttributeKeyVO<? extends Serializable>, Serializable> attributes;
-    
+    private transient boolean _transient;
+
+    /**
+     * The number of times the event occurred.
+     */
     public static final AttributeKeyVO<Long> ATTRIBUTE_COUNT = new AttributeKeyVO<>("count");
+    /**
+     * How long the event lasted, in ms.
+     */
     public static final AttributeKeyVO<Long> ATTRIBUTE_DURATION = new AttributeKeyVO<>("duration");
+    /**
+     * When the event started, in ms since the epoch.
+     */
     public static final AttributeKeyVO<Date> ATTRIBUTE_TIMESTAMP = new AttributeKeyVO<>("timestamp");
 
     private static final long serialVersionUID = 1L;
